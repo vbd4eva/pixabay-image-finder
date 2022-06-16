eb709248dcc8bbe47a69787af0c48b0605e79fc9
Denis Onyshchuk <o.82.denis@gmail.com>
2022-06-16 22:24:14 +0300

    makes function instead class in component  ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { Component } from "react";
 
 import s from "./ImageGallery.module.css";
 
@@ -7,36 +6,73 @@ import ImageGalleryItem from "./ImageGalleryItem/";
 import { RotatingLines } from "react-loader-spinner";
 import statuses from "../../json/statuses.json";
 
-export default class ImageGallery extends Component {
-  static propTypes = {
-    images: PropTypes.arrayOf(
-      PropTypes.shape({ id: PropTypes.number.isRequired })
-    ).isRequired,
-    onGalleryItemClick: PropTypes.func.isRequired,
-    status: PropTypes.string,
-  };
+// class ImageGallery1 {
+//   // static propTypes = {
+//   //   images: PropTypes.arrayOf(
+//   //     PropTypes.shape({ id: PropTypes.number.isRequired })
+//   //   ).isRequired,
+//   //   onGalleryItemClick: PropTypes.func.isRequired,
+//   //   status: PropTypes.string,
+//   // };
 
-  render() {
-    return (
-      <>
-        <ul className={s.gallery}>
-          {this.props.images.map(({ id, webformatURL, tags }, index) => (
-            <ImageGalleryItem
-              key={id}
-              imgSrc={webformatURL}
-              imgAlt={tags}
-              onClick={() => {
-                this.props.onGalleryItemClick(index);
-              }}
-            />
-          ))}
-        </ul>
-        {this.props.status === statuses.PENDING && (
-          <div>
-            <RotatingLines width="24" strokeColor="currentColor" />
-          </div>
-        )}
-      </>
-    );
-  }
+//   render() {
+//     return (
+//       <>
+//         {/* <ul className={s.gallery}>
+//           {this.props.images.map(({ id, webformatURL, tags }, index) => (
+//             <ImageGalleryItem
+//               key={id}
+//               imgSrc={webformatURL}
+//               imgAlt={tags}
+//               onClick={() => {
+//                 this.props.onGalleryItemClick(index);
+//               }}
+//             />
+//           ))}
+//         </ul>
+//         {this.props.status === statuses.PENDING && (
+//           <div>
+//             <RotatingLines width="24" strokeColor="currentColor" />
+//           </div>
+//         )} */}
+//       </>
+//     );
+//   }
+// }
+//
+//
+//
+
+function ImageGallery({ onGalleryItemClick, status, images }) {
+  return (
+    <>
+      <ul className={s.gallery}>
+        {images.map(({ id, webformatURL, tags }, index) => (
+          <ImageGalleryItem
+            key={id}
+            imgSrc={webformatURL}
+            imgAlt={tags}
+            onClick={() => {
+              onGalleryItemClick(index);
+            }}
+          />
+        ))}
+      </ul>
+      {status === statuses.PENDING && (
+        <div>
+          <RotatingLines width="24" strokeColor="currentColor" />
+        </div>
+      )}
+    </>
+  );
 }
+
+ImageGallery.propTypes = {
+  onGalleryItemClick: PropTypes.func.isRequired,
+  status: PropTypes.string,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number.isRequired })
+  ).isRequired,
+};
+
+export default ImageGallery;
